e89a8dfd8da4225f31cf49cd75f0a4110496e131
Peggy-1111 <A112223001@mail.shu.edu.tw>
2024-10-18 12:00:06 +0800

    113-10-18

---
+++ 113-10-18/HW1/src/Time1.java
@@ -0,0 +1,12 @@
+public class Time1 {
+    //時(hour)
+    //分(minute)
+    //秒(second)
+
+    //建構子(設定時、分、秒的初始直，需檢查資是否格是正確)
+    //方法:是需要加入get set
+
+    //方法:tick(時間增加一秒)
+    //方法:printUniversal(以24小時至輸出完整時間)
+
+}
