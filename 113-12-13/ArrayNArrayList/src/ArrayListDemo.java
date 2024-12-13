af4dee0b831910053943c5ba62d42b1da4f88ddb
Peggy-1111 <A112223001@mail.shu.edu.tw>
2024-12-13 12:02:21 +0800

    113-12-13

---
+++ 113-12-13/ArrayNArrayList/src/ArrayListDemo.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[]args){
+        ArrayList<String> list=new ArrayList;
+
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+
+    }
+}
