b28c202661361af16192de07df2b69e322905f99
Peggy-1111 <A112223001@mail.shu.edu.tw>
2024-12-20 11:54:13 +0800

    113-12-20

---
+++ 113-12-20/LibrarySystem/src/PhysicalBook.java
@@ -0,0 +1,15 @@
+public class PhysicalBook extends Book {
+    private String shelfLocation;
+
+    public PhysicalBook(String title,String author,String shelfLocation){
+        super(title,author);
+        this.shelfLocation=shelfLocation;
+    }
+    public String getShelfLocation(){
+        return shelfLocation;
+    }
+    @Override
+    public String toString(){
+        return super.toString()+",書架位置"+shelfLocation;
+    }
+}
